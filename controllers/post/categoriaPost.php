c471a58cb86af9d071559f1556881364d4a0fd52
WestAMZ <westy15@hotmail.com>
2016-07-08 09:19:56 -0600

    modificacion a bd y post categoria

--- controllers/post/categoriaPost.php
+++ controllers/post/categoriaPost.php
@@ -1,3 +1,27 @@
 <?php
+    if(!isset($_GET['mod']))
+        {
+            $categoria = new Categoria($_GET['nombre'],$_GET['descripcion'],null);
 
+            if($categoria->saveCategoria())
+            {
+                echo ('1');
+            }
+            else
+            {
+                echo ($categoria->add_error());
+            }
+        }
+        //mode 1 : update
+        else if($_GET['mod']==1)
+        {
+            $categoria = new Categoria($_GET['id_categoria'],$_GET['nombre'],$_GET['descripcion']);
+            $categoria->update();
+        }
+        //mode 2: cambio de estado
+        else if($_GET['mod']==2)
+        {
+            Categoria::cambiarEstado($_GET['id'],$_GET['estado']);
+            echo('1');
+        }
 ?>
