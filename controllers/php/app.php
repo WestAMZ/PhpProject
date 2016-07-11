293ddcc16f235cb0dd9f829bf460171442c0770c
ricardo04 <remartinezc1@hotmail.com>
2016-07-11 12:23:32 -0600

    insidencia sitio

--- controllers/php/app.php
+++ controllers/php/app.php
@@ -117,17 +117,18 @@
             include(HTML_DIR . 'subcategoria.html');
 
         }
-<<<<<<< HEAD
         static function getMenuSitio()
         {
             include(HTML_DIR . 'seguimiento.html');
         }
 
-=======
         static function getPermisos()
         {
             include(HTML_DIR . 'permisos.html');
         }
->>>>>>> origin/master
+        static function getInsidenciaSitio()
+        {
+            include(HTML_DIR . 'insidenciasitio.html');
+        }
     }
 ?>
---
+++ controllers/php/insidenciasitioController.php
@@ -0,0 +1,13 @@
+<?php
+    App::getHead('Seguimientos del sitio');
+?>
+
+
+<div id="main-container" class="container-fluid row full">
+
+
+<?php
+    App::getLeftMain();
+    App::getRightMain();
+    App::getInsidenciaSitio();
+?>
