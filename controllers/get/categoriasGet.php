49787e9ca2556d6026bfaa96159e638b23cd0ff0
ricardo04 <remartinezc1@hotmail.com>
2016-07-11 07:26:00 -0600

    alineando menus

--- controllers/get/categoriasGet.php
+++ controllers/get/categoriasGet.php
@@ -4,7 +4,8 @@
      foreach ($categorias as &$categoria)
     {
 ?>
-    <a class="menu-btn categoria col s11 m4 offset-m1" href="?view=menusubcategoria&url=<?php echo($categoria->getUrl())?>" id="<?php echo($categoria->getIdCategoria())?>">
+
+    <a class="menu-btn categoria col s11 m3 offset-s1 offset-m1" href="?view=menusubcategoria&url=<?php echo($categoria->getUrl())?>" id="<?php echo($categoria->getIdCategoria())?>">
         <div class="menu-div">
             <img src="<?php echo(IMG_DIR . $categoria->getImg())?>" alt="">
         </div>
