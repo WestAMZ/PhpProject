86bbf32cd3a721dc3d4a5e2a871b917535d01e58
ricardo04 <remartinezc1@hotmail.com>
2016-07-11 14:04:00 -0600

    casi listo las insidencias

--- controllers/get/insidenciatableGet.php
+++ controllers/get/insidenciatableGet.php
@@ -31,33 +31,56 @@
 }
 else
 {
-    $insidencias = Insidencia::getInsidencias();
-    foreach ($insidencias as &$insidencia)
-    {
+            $id = $_GET['id'];
+            Connection :: connect();
+            $query = "SELECT i.id_insidencia,
+	                         i.estado,terter
+	                         i.fecha,
+	                         i.titulo,
+	                         i.descripcion,
+	                         CONCAT(e.nombre1, ' ', e.apellido2) AS 'usuario'
+                      FROM usuario u INNER JOIN insidencia i ON u.id_usuario = i.id_usuario
+                      INNER JOIN empleado e on u.id_empleado = e.id_empleado
+                      INNER JOIN sitio s ON e.id_sitio = s.id_sitio WHERE s.id_sitio = '$id' ORDER BY i.fecha DESC";
+
+            $result = Connection::getConnection()->query($query);
+            while($row = $result->fetch_assoc())
+            {
 
 ?>
 
-        <tr id="<?php echo($insidencia->getIdInsidencia())?>" class="insidencia">
+        <tr id="<?php echo($row['id_insidencia'])?>" class="insidencia">
             <td>
-                <?php echo($insidencia->getIdInsidencia())?>
+                <?php echo($row['id_insidencia'])?>
             </td>
             <td>
-                <?php echo($insidencia->getEstado())?>
+                <?php
+                    if($row['estado'] == 1)
+                    {
+                        $img = 'locked.svg';
+                    }
+                    else
+                    {
+                        $img = 'locked-1.svg';
+                    }
+                 ?>
+                 <img src="<?php IMG_DIR . $img ?>">
             </td>
             <td>
-                <?php echo($insidencia->getFecha())?>
+                <?php echo($row['fecha'])?>
             </td>
             <td>
-                <?php echo($insidencia->getTitulo())?>
+                <?php echo($row['titulo'])?>
             </td>
             <td>
-                <?php echo($insidencia->getDescripcion())?>
+                <?php echo($row['descripcion'])?>
             </td>
             <td>
-                <?php echo($insidencia->getId_Usuario())?>
+                <?php echo($row['usuario'])?>
             </td>
         </tr>
         <?php
+            }
+             Connection ::close();
     }
-}
 ?>
