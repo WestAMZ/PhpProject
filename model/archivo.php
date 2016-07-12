2d5db2bd99873b8f3f16706ae5b93c2c0b297b09
WestAMZ <westy15@hotmail.com>
2016-07-12 07:43:16 -0600

    Metodo de busqueda de archivo

--- model/archivo.php
+++ model/archivo.php
@@ -156,7 +156,8 @@
         static function searchArchivos($search,$id_subcategoria)
         {
             Connection :: connect();
-            $query = "SELECT id_archivo,fecha_subida, descripcion, nombre, id_subcategoria, id_usuario,estado FROM archivo WHERE  descripcion LIKE '%$search%' OR nombre LIKE '%$search%' ";
+            $query = "SELECT id_archivo,fecha_subida, descripcion, nombre, id_subcategoria, id_usuario,estado FROM archivo WHERE id_subcategoria ='$id_subcategoria' AND (descripcion LIKE '%$search%' OR nombre LIKE '%$search%')";
+            echo($query);
             $result = Connection :: getConnection()->query($query);
 
             $archivos = array();
@@ -170,10 +171,10 @@
                 $archivo->setIdSubcategoria($row['id_subcategoria']);
                 $archivo->setIdUsuario($row['id_usuario']);
                 $archivo->setEstado($row['estado']);
-                if($archivo->setIdSubcategoria($row['id_subcategoria']) == $id_subcategoria)
-                {
+                /*if($archivo->setIdSubcategoria($row['id_subcategoria']) == $id_subcategoria)
+                {*/
                     array_push($archivos, $archivo);
-                }
+                /*}*/
             }
             Connection :: close();
             return $archivos;
