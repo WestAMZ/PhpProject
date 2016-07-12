fd883992e1583429fcabd89a24c5a7c47b28c479
ricardo04 <remartinezc1@hotmail.com>
2016-07-12 13:14:08 -0600

    permisos

--- model/acceso.php
+++ model/acceso.php
@@ -40,4 +40,28 @@ function getPermiso()
 }
 
 }
+static function getAccesoByEmpleadoAndCategoria($id_empleado, $id_categoria)
+{
+    Connection :: connect();
+    $query = "SELECT a.id_acceso,
+                     a.permiso,
+	                 a.id_subcategoria,
+                     a.usuario_id_usuario
+              FROM usuario u INNER JOIN acceso a ON u.id_usuario = a.usuario_id_usuario
+              INNER JOIN sub_categoria sc ON a.id_subcategoria = sc.id_subcategoria
+              WHERE e.id_empleado = '$id_empleado' AND c.id_categoria = $id_categoria";
+    $result = Connection :: getConnection()->query($query);
+    $accesos = array();
+    while($row = $result->fetch_assoc())
+    {
+        $acceso = new Acceso();
+        $acceso->setIdAcceso($row['id_acceso']);
+        $acceso->setIdSubcategoria($row['id_subcategoria']);
+        $acceso->setIdUsuario($row['usuario_id_usuario']);
+        $acceso->setPermiso($row['permiso']);
+        array_push($accesos, $acceso);
+    }
+    Connection :: close();
+    return $accesos;
+}
 ?>
