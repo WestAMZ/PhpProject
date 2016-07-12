79f66791e08d22104299588983f6fb4f22b2105b
ricardo04 <remartinezc1@hotmail.com>
2016-07-11 20:09:56 -0600

    incidencias

--- controllers/ajax/seguimientositio.js
+++ controllers/ajax/seguimientositio.js
@@ -18,7 +18,26 @@ $(document).ready(function ()
     });
 
 });
+$("#agregarinsidencia").submit(function ()
+{
+    var data = $("#agregarinsidencia").serialize();
+    var archivo = new FormData();
+    if($('#file')[0].files.length > 0)
+    {
+        archivo.append('archivo', $('#file')[0].files[0]);
+        var nombre_archivo = $('#file')[0].files[0].name;
+        nombre_archivo = nombre_archivo.substring(0,nombre_archivo.indexOf('.'));
+        archivo.append('nombre_archivo', nombre_archivo);
+    }
+    archivo.append('ruta', "sources/public/files/");
+    result = $('#result');
+    var ms = $('#message');
+    var modal = $('#myModal');
 
+    agregarinsidencia(archivo ,data, result, modal, ms);
+  //  loadInsidencias($('#insidencias'),result,modal,ms);
+    return false;
+});
 
 function searchSitios(search, table) {
     httpL = Connect();
@@ -35,4 +54,40 @@ function searchSitios(search, table) {
     httpL.open('GET', '?get=seguimientositios&search=' + search);
     httpL.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     httpL.send(null);
+}
+function agregarinsidencia(archivo,data,result,modal,message_area_modal)
+{
+    $.ajax(
+    {
+        url: '?post=insidencia&'+data,
+        type: 'POST',
+        data: archivo,
+        dataType: "html",
+        cache: false,
+        contentType: false,
+        processData: false,
+        complete: function(res)
+                    {
+                        var json;
+                        try
+                        {
+                            if(res.responseText==1)
+                            {
+                                message_area_modal.html("<img src='views/img/success.png'></img> la insidencia ha sido posteada");
+                                modal.openModal();
+                                result.html('');
+                            }
+                            else
+                            {
+                                text = '<div class="alert alert-dismissible alert-danger">' +
+                                '<button type="button" class="close" data-dismiss="alert">&times;</button>' + http.responseText + '</div>';
+                                result.html(http.responseText);
+                            }
+                        }
+                        catch (e)
+                        {
+                            $('#result').html(res.responseText);
+                        }
+                    }
+    });
 }
